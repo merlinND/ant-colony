3cfa24c65c6525bc8c8d28b507c30d8a247d65e6
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-07-06 15:52:34 -0700

    Add Gruntfile with browserify, express and watch confiigs

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        express: {
+            options: {
+                script: 'index.js'
+            },
+            dev : {
+
+            }
+        },
+        browserify: {
+            dist: {
+                files: {
+                    'static/js/app.js': ['client/app.js']
+                },
+                options: {
+                    transform: []
+                }
+            },
+        },
+        watch: {
+            // files: ['**/*.js', '!static/js/*'],
+            express: {
+              // files:  [ '**/*.js' ],
+              files:  [ 'index.js' ],
+              tasks:  [ 'express:dev' ],
+              options: {
+                // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+                spawn: false
+              },
+            },
+            browserify: {
+              files:  [ 'client/**/*.js' ],
+              tasks:  [ 'browserify' ],
+            },
+            // tasks: ['browserify']
+        },
+    })
+
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('default', ['watch']);
+};
