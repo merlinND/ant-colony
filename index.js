316a39e0ebde1a5316bd311f037d61fcd50af5d7
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-07-06 15:53:44 -0700

    Main app page mockup + Phaser game playground
    The Phaser game follows this tutorial: https://phaser.io/tutorials/making-your-first-phaser-3-game/

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
 
+// ----- Configuration
 const app = express()
 const port = 3000
-
 
 app.engine('.hbs', exphbs({
     extname: 'hbs',
 }));
 app.set('view engine', '.hbs');
 
-app.use(express.static('static'))
 
+// ----- Static files
+app.use(express.static('static'))
+// Static files included in NPM modules
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/phaser/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css', express.static(__dirname + '/node_modules/codemirror/lib'));
+app.use('/css', express.static(__dirname + '/node_modules/codemirror/theme'));
+
+// ----- Routes
 app.get('/', (req, res) => {
-    res.render('index', { name:'John' })
+    res.render('index', { })
+})
+app.get('/playground', (req, res) => {
+    res.render('playground', { })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`Ant Colony server listening on port ${port}!`))
