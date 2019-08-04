e8aaf99ccc43d3609a0a439d46fab070916f232e
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-08-04 11:03:31 -0700

    Eval user code and show errors inline

--- client/app.js
+++ client/app.js
@@ -10,6 +10,69 @@ CodeMirrorPersist(CodeMirror)
 const sideScrollerPlayground = require("./sideScrollerPlayground.js")
 const topDownPlayground = require("./topDownPlayground.js")
 
+const executeUserCode = function(user_code, editor) {
+    clearCodeErrors(editor);
+    try {
+        eval(user_code);
+    } catch (e) {
+        showCodeError(editor, e);
+    }
+};
+
+const clearCodeErrors = function(editor) {
+    if (!editor.widgets)
+        return;
+
+    for (var i = 0; i < editor.widgets.length; ++i) {
+        editor.removeLineWidget(editor.widgets[i]);
+    }
+    editor.widgets.length = 0;
+}
+
+const showCodeError = function(editor, exception) {
+    if (!editor.widgets)
+        editor.widgets = []
+
+    // Find offending line in user code
+    const message = exception.message;
+    const lineRegex = /eval .+ <anonymous>:(\d+)/;
+    var exceptionLine;
+    try {
+        var matches = exception.stack.match(lineRegex);
+        if (matches.length > 1) {
+            exceptionLine = parseInt(matches[1]);
+        } else {
+            exceptionLine = 0;
+        }
+    } catch (e) {
+        exceptionLine = 0;
+    }
+
+    console.log('Code error at line ' + exceptionLine + ': ' + message);
+
+    editor.operation(function(){
+        if (!exception) return;
+
+        var msg = document.createElement("div");
+        var icon = msg.appendChild(document.createElement("span"));
+        icon.innerHTML = "Error: ";
+        icon.className = "code-error-icon";
+        msg.appendChild(document.createTextNode(message));
+        msg.className = "code-error text-danger";
+
+        var widget = editor.addLineWidget(exceptionLine - 1, msg, {
+            coverGutter: false, noHScroll: true
+        });
+        editor.widgets.push(widget);
+    });
+
+    // TODO: consider scrolling to first error
+    // var info = editor.getScrollInfo();
+    // var after = editor.charCoords({line: editor.getCursor().line + 1, ch: 0}, "local").top;
+    // if (info.top + info.clientHeight < after)
+    //   editor.scrollTo(null, after - info.clientHeight + 3);
+}
+
 
 const initAntsApp = function(container) {
     console.log('Initializing ants app (editor + game)');
@@ -28,8 +91,12 @@ const initAntsApp = function(container) {
         persist: true // enable buffer persistence
     });
 
+    // For debugging
+    window.code_editor = editor;
+
     $('button[name="play"]').on('click', function() {
-        console.log(editor.getValue());
+        const user_code = editor.getValue();
+        executeUserCode(user_code, editor);
     })
 
 
