bcea0893d01b1b3664e676459c37c4718fa3c88a
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-09 19:21:21 +0200

    Grunt: also watch on locale files

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
             // files: ['**/*.js', '!static/js/*'],
             express: {
               // files:  [ '**/*.js' ],
-              files:  [ 'index.js' ],
+              files:  [ 'index.js', 'locales/*.json' ],
               tasks:  [ 'express:dev' ],
               options: {
                 // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
