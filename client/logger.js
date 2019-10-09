e309e9f86034f79486fc76137472c899ebe96cab
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-09 20:35:10 +0200

    Logger: support warn & error

--- client/logger.js
+++ client/logger.js
@@ -5,22 +5,41 @@ const init = function() {
     div = document.getElementById("logger-window")
 }
 
-const print = function(msg) {
+const clear = function () {
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+const print = function(msg, type) {
+    type = type || "status";
+
     if (div.childElementCount >= 100) {
         div.removeChild(div.firstChild);
     }
     msgDiv = document.createElement("div");
+    msgDiv.className += " " + type;
     msgDiv.innerHTML = msg;
     div.appendChild(msgDiv);
 
     let autoScroll = ( div.scrollHeight - (div.scrollTop+div.offsetHeight) < msgDiv.offsetHeight)
     if(autoScroll==true) {
-        div.scrollTop = div.scrollHeight;    
+        div.scrollTop = div.scrollHeight;
     }
-    
+}
+
+const warn = function(msg) {
+    return print(msg, "warn");
+}
+
+const error = function(msg) {
+    return print(msg, "error");
 }
 
 module.exports = {
     init: init,
-    print:print,
+    clear: clear,
+    print: print,
+    warn: warn,
+    error: error,
 }
